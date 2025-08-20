de9ff3ba70b988e6a880d43ca268625858bd8511
moecode-c <mohammedessameldincs@gmail.com>
2025-08-20 19:51:31 +0300

    vite2

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/3dportfolio"
+  base: process.env.VITE_BASE_PATH || "/3dportfolio",
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets')
+    }
+  }
 })
